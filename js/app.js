3d1e28cdc212b3618bdd53c8afcb2edcb8fd642b
LaurenceWang <61631525+LaurenceWang@users.noreply.github.com>
2020-09-30 14:49:52 +0200

    plateau complet

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
-for(var y=0; y<17; y++){ 
+for(var y=0; y<19; y++){ 
 
     for(var i=0; i<17; i++){
         $(".plateau").append("<div class='case'> </div>")
 
+        if(y%2==0){ 
+            if(i%2==0){ 
+                $(".case").eq((y*17)+i).css({"width":"60","height":"10","background-color":"green"});
+               }
+       
+               else{
+                   $(".case").eq((y*17)+i).css({"width":"10","height":"10","background-color":"green"})
+               }
+        }
+
+        else{ 
         if(i%2==0){ 
          $(".case").eq((y*17)+i).css({"width":"60","height":"60","background-color":"rgb(190, 126, 89)"});
         }
@@ -10,7 +21,8 @@ for(var y=0; y<17; y++){
         else{
             $(".case").eq((y*17)+i).css({"width":"10","height":"60","background-color":"green"})
         }
-    }
+        }
+}
      
     }    
     
