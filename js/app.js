3930040da5aad18ad85bb2c5748f1c6feb1f1688
Théo Buffet <62326624+TheoBuff@users.noreply.github.com>
2020-10-22 14:30:53 +0200

    h

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ for (let y = 0; y < 19; y++) {
                 $(".rectangle").eq((y * 17) + i).css({
                     "width": "60",
                     "height": "10",
-                  
+
                 });
 
             } else {
@@ -26,7 +26,7 @@ for (let y = 0; y < 19; y++) {
                 $(".rectangle").eq((y * 17) + i).css({
                     "width": "10",
                     "height": "10",
-                    
+
                 })
 
             }
@@ -47,7 +47,7 @@ for (let y = 0; y < 19; y++) {
                 $(".rectangle").eq((y * 17) + i).css({
                     "width": "10",
                     "height": "60",
-                   
+
                 })
             }
         }
@@ -56,11 +56,36 @@ for (let y = 0; y < 19; y++) {
     }
 
 
+
+}
+for (let a = 0; a < 17; a++) {
+
+
+
+    $(".storageUn").append(`<div class='murStorage'></div>`)
+
+    if (a % 2 == 0) {
+
+        $(".murStorage").eq(a).css({
+            "width": "60",
+            "height": "130",
+
+        });
+
+    } else {
+
+        $(".murStorage").eq(a).css({
+            "width": "10",
+            "height": "130",
+
+        })
+
+    }
 }
 
 
 let voisin = [];
-let mvoisin=[];
+let mvoisin = [];
 
 $("#76").addClass('jouable');
 $("#78").addClass('jouable');
@@ -68,44 +93,42 @@ $("#68").addClass('jouable');
 $(".jouable").addClass("pionShadow");
 
 $("body").on("click", ".jouable", function () {
-   
+
     $(".jouable").removeClass('pionShadow');
     $(".jouable").removeClass('jouable');
-   
-    if($(".pion1").hasClass('active')){
-    $(".pion1").removeClass('pion1');
-    $(".active").removeClass('active')
-    $(".pion2").addClass('active');
-    $(this).addClass('pion1');
+
+    if ($(".pion1").hasClass('active')) {
+        $(".pion1").removeClass('pion1');
+        $(".active").removeClass('active')
+        $(".pion2").addClass('active');
+        $(this).addClass('pion1');
 
 
-}
+    } else if ($(".pion2").hasClass('active')) {
+        $(".pion2").removeClass('pion2');
+        $(".active").removeClass('active');
+        $(".pion1").addClass('active');
+        $(this).addClass('pion2');
 
-else if($(".pion2").hasClass('active')){  
-    $(".pion2").removeClass('pion2');
-    $(".active").removeClass('active');
-    $(".pion1").addClass('active');
-    $(this).addClass('pion2');
-    
-  
-}
 
-    voisin = [$(".active").attr("data-vgauche"),$(".active").attr("data-vhaut"), $(".active").attr("data-vdroite"),$(".active").attr("data-vbas")];
-    $("#"+voisin[0]).addClass('jouable');
-    $("#"+voisin[1]).addClass('jouable');
-    $("#"+voisin[2]).addClass('jouable');
-    $("#"+voisin[3]).addClass('jouable');
-    console.log(voisin); 
+    }
 
-    
+    voisin = [$(".active").attr("data-vgauche"), $(".active").attr("data-vhaut"), $(".active").attr("data-vdroite"), $(".active").attr("data-vbas")];
+    $("#" + voisin[0]).addClass('jouable');
+    $("#" + voisin[1]).addClass('jouable');
+    $("#" + voisin[2]).addClass('jouable');
+    $("#" + voisin[3]).addClass('jouable');
+    console.log(voisin);
 
-    if($(".jouable").hasClass("pion1") || $(".jouable").hasClass("pion2")){
+
+
+    if ($(".jouable").hasClass("pion1") || $(".jouable").hasClass("pion2")) {
         $(".pion1").removeClass("jouable");
         $(".pion2").removeClass("jouable");
     }
-$(".jouable").addClass("pionShadow");
-    
-}); 
+    $(".jouable").addClass("pionShadow");
+
+});
 
 
 
@@ -114,64 +137,58 @@ $("#77").addClass('active');
 $("#5").addClass('pion2');
 
 
-$(".mur").on("click", function(){
-$(".jouable").removeClass('pionShadow');
-$(".jouable").removeClass('jouable');
-    mvoisin = [$(this).attr("data-mgauche"),$(this).attr("data-mhaut"), $(this).attr("data-mdroite"),$(this).attr("data-mbas"),];
+$(".mur").on("click", function () {
+    $(".jouable").removeClass('pionShadow');
+    $(".jouable").removeClass('jouable');
+    mvoisin = [$(this).attr("data-mgauche"), $(this).attr("data-mhaut"), $(this).attr("data-mdroite"), $(this).attr("data-mbas"), ];
 
 
-    if($(this).hasClass("horizontal") && $(".pion1").hasClass("active")){
+    if ($(this).hasClass("horizontal") && $(".pion1").hasClass("active")) {
         $(this).addClass("murSolid");
         $("#mur" + mvoisin[2]).addClass("murSolid");
         let prochainHmur = $("#mur" + mvoisin[2]).attr("data-mdroite");
-        $("#mur" + prochainHmur ).addClass("murSolid");
+        $("#mur" + prochainHmur).addClass("murSolid");
 
-          $(".pion1").removeClass("active");
+        $(".pion1").removeClass("active");
         $(".pion2").addClass("active");
-      
-       
-    }
 
-    else if($(this).hasClass("horizontal") && $(".pion2").hasClass("active")){
+
+    } else if ($(this).hasClass("horizontal") && $(".pion2").hasClass("active")) {
         $(this).addClass("murSolid");
         $("#mur" + mvoisin[2]).addClass("murSolid");
         let prochainHmur = $("#mur" + mvoisin[2]).attr("data-mdroite");
-        $("#mur" + prochainHmur ).addClass("murSolid");
+        $("#mur" + prochainHmur).addClass("murSolid");
 
         $(".pion2").removeClass("active");
         $(".pion1").addClass("active");
-      
-       
-    }
 
-    else if($(this).hasClass("vertical") && $(".pion1").hasClass("active")){
+
+    } else if ($(this).hasClass("vertical") && $(".pion1").hasClass("active")) {
 
         $(this).addClass("murSolid");
-         $("#mur" + mvoisin[3]).addClass("murSolid");
-         
+        $("#mur" + mvoisin[3]).addClass("murSolid");
+
         //  let prochainVMur = $("#mur" + mvoisin[3]).attr("data-mbas");
         //  $("#mur" + prochainVMur ).addClass("murSolid");
         $(".pion1").removeClass("active");
         $(".pion2").addClass("active");
-    }
-
-    else if($(this).hasClass("vertical") && $(".pion2").hasClass("active")){
+    } else if ($(this).hasClass("vertical") && $(".pion2").hasClass("active")) {
 
         $(this).addClass("murSolid");
-         $("#mur" + mvoisin[3]).addClass("murSolid");
-         
+        $("#mur" + mvoisin[3]).addClass("murSolid");
+
         //  let prochainVMur = $("#mur" + mvoisin[3]).attr("data-mbas");
         //  $("#mur" + prochainVMur ).addClass("murSolid");
         $(".pion2").removeClass("active");
         $(".pion1").addClass("active");
     }
 
-    
-        voisin = [$(".active").attr("data-vgauche"),$(".active").attr("data-vhaut"), $(".active").attr("data-vdroite"),$(".active").attr("data-vbas")];
-        $("#"+voisin[0]).addClass('jouable');
-        $("#"+voisin[1]).addClass('jouable');
-         $("#"+voisin[2]).addClass('jouable');
-        $("#"+voisin[3]).addClass('jouable');
-        $(".jouable").addClass("pionShadow");
+
+    voisin = [$(".active").attr("data-vgauche"), $(".active").attr("data-vhaut"), $(".active").attr("data-vdroite"), $(".active").attr("data-vbas")];
+    $("#" + voisin[0]).addClass('jouable');
+    $("#" + voisin[1]).addClass('jouable');
+    $("#" + voisin[2]).addClass('jouable');
+    $("#" + voisin[3]).addClass('jouable');
+    $(".jouable").addClass("pionShadow");
 
 });
